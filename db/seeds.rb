9194eb07a42e81fbf7fa270577aca585873d5cfa
zora dong <zoradongccc@gmail.com>
2022-08-17 14:47:12 +0800

    fixed seed file & add wechat user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ trip_url="https://api.unsplash.com/search/photos?page=1&query=group+trip&client_
 trips = JSON.parse(URI.open(trip_url).read)["results"].first(10)
 
 ## TRIP
-TITLE = %w["Hiking in the mountains", "Yoga & Meditation Retreat", "Camping in the forest", "Memorable journey to the scenic sites", "Rascals on the road - Waterfalls", "Weekend getaway", "Countrywalk for the city dwellers", "Weekend in bamboo forest & reservoir", "Stand-up paddling in the lake", "Escape the city life - Mindful Retreat" ]
-CITY = %w["Moganshan", "Anhui", "Anji", "Ningbo", "Chongming", "Moganshan", "Xianju", "Hangzhou", "Anji", "Moganshan"]
+TITLE = ["Hiking in the mountains", "Yoga & Meditation Retreat", "Camping in the forest", "Memorable journey to the scenic sites", "Rascals on the road - Waterfalls", "Weekend getaway", "Countrywalk for the city dwellers", "Weekend in bamboo forest & reservoir", "Stand-up paddling in the lake", "Escape the city life - Mindful Retreat" ]
+CITY = ["Moganshan", "Anhui", "Anji", "Ningbo", "Chongming", "Moganshan", "Xianju", "Hangzhou", "Anji", "Moganshan"]
 
 TAGS = %w[Hiking One-day Weekend Pet-friendly Family Relaxing Adventure Biking]
 
@@ -82,7 +82,7 @@ end
 # CITY = ["Moganshan", "Anhui", "Anji", "Ningbo", "Chongming", "Moganshan", "Xianju", "Hangzhou", "Anji", "Moganshan"]
 # TAGS = %w[Hiking One-day Weekend Pet-friendly Family Relaxing Adventure Biking]
 
-# id = 16
+# id = 27 # change to your own wechat user id
 
 # user = User.find(id)
 # %w[open ongoing closed].each_with_index do |status, i|
@@ -100,5 +100,22 @@ end
 #     trip.tags << Tag.all.sample(rand(1..3))
 #     # trip.save
 #     p "Added new trip for wechat user: #{trip.title}"
+#   end
+# end
+
+# 5.times do
+#   # booking
+#   if b = Booking.create(user: user, trip: Trip.all.sample)
+#     p "add new booking."
+#   else
+#     p "fail to add new booking"
+#   end
+#   # bookmark
+#   User.all.each do |user|
+#     if b = Bookmark.create(user: user, trip: Trip.all.sample)
+#       p "add new bookmark."
+#     else
+#       p "fail to add new bookmark"
+#     end
 #   end
 # end
